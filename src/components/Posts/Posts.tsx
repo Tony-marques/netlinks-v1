91719e5887a7172c68478ab9c77bf11b13bdaa34
tonym <tony.marques@live.fr>
2024-02-29 21:30:43 +0100

    Add Modal on PostPage

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -50,7 +50,6 @@ export default Posts;
 const PostsStyled = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: center;
     gap: 1rem;
 
 
--- src/pages/PostsPage/PostsPage.tsx
+++ src/pages/PostsPage/PostsPage.tsx
@@ -1,10 +1,32 @@
 import {styled} from "styled-components";
 import Posts from "../../components/Posts/Posts.tsx";
+import {useAuth} from "../../hooks/useAuth.tsx";
+import {useState} from "react";
+import Modal from "../../components/Posts/components/Modal/Modal.tsx";
 
 const PostsPage = () => {
+    const [showModal, setShowModal] = useState<boolean>(false);
+    const {account} = useAuth();
+
+    const handleOnClick = () => {
+        console.log("click");
+        setShowModal(true);
+    };
+
+    const toggleModal = (toggle: boolean) => {
+        setShowModal(toggle)
+    }
 
     return (
         <PostsPageStyled>
+            <div className="whatsUp">
+                <img
+                    src={"/public/assets/images/default-profil.jpg"}
+                    alt=""
+                />
+                <span onClick={handleOnClick}>Quoi de neuf ? {account.email}</span>
+                {showModal && <Modal toggleModal={toggleModal}/>}
+            </div>
             <Posts/>
         </PostsPageStyled>
     );
@@ -13,5 +35,36 @@ const PostsPage = () => {
 export default PostsPage;
 
 const PostsPageStyled = styled.div`
-    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .whatsUp {
+        padding: 1rem;
+        width: 600px;
+        background-color: #242526;
+        //color: #E0E1E6;
+        border-radius: 5px;
+        display: flex;
+        gap: 1rem;
+        align-items: center;
+        margin-bottom: 1rem;
+
+
+        img {
+            width: 40px;
+            height: 40px;
+            object-fit: cover;
+            border-radius: 50%;
+        }
+
+        span {
+            flex: 1;
+            background-color: #3A3B3C;
+            padding: 0.5rem;
+            border-radius: 30px;
+            color: #B0B3B8;
+            cursor: pointer;
+        }
+    }
 `;
